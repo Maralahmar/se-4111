d25c085d6e81224672ef26ad280aaabfdd36e145
MaryaIB <eemm.i.b@gmail.com>
2022-10-30 23:13:29 +0300

    print my name

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ public class main {
 		// TODO Auto-generated method stub
 		singletonClass tmp = singletonClass.getInstance( );
 	      tmp.demoMethod( );
+	      System.out.println("Mariam Alahmar");
 	}
 
 }
