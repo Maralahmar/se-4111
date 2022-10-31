c9046aff48083ab2fb33d5c664941975a6d97534
Hadeel939 <deloo939@gmail.com>
2022-10-31 16:58:27 +0300

    first edit

--- main.java
+++ main.java
@@ -7,6 +7,7 @@ public class main {
 		singletonClass tmp = singletonClass.getInstance( );
 	      tmp.demoMethod( );
 	      System.out.println("Mariam Alahmar");
+	      Ssystem.out.println("Hadeel Abdullah");
 	}
 
 }
