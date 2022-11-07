a32a4036f5637d289822e42266fe41fe5d912b26
Hadeel939 <deloo939@gmail.com>
2022-11-08 00:53:02 +0300

    test edit

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		singletonClass tmp = singletonClass.getInstance( );
-	      tmp.demoMethod( );
+	    
 	      System.out.println("Mariam Alahmar");
 	      System.out.println("Hadeel Abdullah");
 	      System.out.println("Leena Abdullah");
